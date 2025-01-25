8eeca078e140aec2c1f9174f0cb0c181b1d801c4
asdfash <jit.tham@gmail.com>
2025-01-25 16:12:45 +0800

    made enemy dissapear

--- bubble_jam/src/enemyPaddle/enemy_paddle.gd
+++ bubble_jam/src/enemyPaddle/enemy_paddle.gd
@@ -1,10 +1,16 @@
-extends CharacterBody2D
+extends Area2D
+signal hit
+#var ball : CharacterBody2D
 
-var ball : CharacterBody2D
+#func _ready() -> void:
+	#ball = get_parent().get_node("ball")
 
-func _ready() -> void:
-	ball = get_parent().get_node("ball")
+#func _physics_process(delta: float) -> void:
+	#global_position.y  = ball.global_position.y
 
-
-func _physics_process(delta: float) -> void:
-	global_position.y  = ball.global_position.y
+func _on_body_entered(_body: Node2D) -> void:
+	hide() # Player disappears after being hit.
+	hit.emit()
+	# Must be deferred as we can't change physics properties on a physics callback.
+	$CollisionShape2D.set_deferred("disabled", true)
+	print("+1")
--- bubble_jam/src/playerPaddle/player_paddle.gd
+++ bubble_jam/src/playerPaddle/player_paddle.gd
@@ -1,11 +1,11 @@
 extends CharacterBody2D
 
-const SPEED := 800.0
+const SPEED := 16000.0
 
 func getYDir() -> float:
 	return Input.get_action_strength("player_down") - Input.get_action_strength("player_up")
 
 func _physics_process(delta: float) -> void:
 	var dir :Vector2=Vector2(0, getYDir())
-	velocity = dir * SPEED
+	velocity = dir * SPEED * delta
 	move_and_slide()
